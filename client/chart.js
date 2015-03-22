b7d35eea76f97c7aaf093976691613d5f7f9f655
Kris Hamoud <khamoud@umich.edu>
2015-03-21 23:53:49 -0700

    added iron router

--- client/chart.js
+++ client/chart.js
@@ -1,4 +1,4 @@
-Template.charts.rendered = function() {
+Template.charts.onRendered(function() {
     // Get the context of the canvas element we want to select
     var ctx  = document.getElementById("myChart").getContext("2d");
     var ctx2 = document.getElementById("myChart2").getContext("2d");
@@ -223,4 +223,29 @@ Template.charts.rendered = function() {
     var myDoughnutChart = new Chart(ctx5).Doughnut(data4,{
         animateScale: true
     });
-}
+});
+
+Template.moreCharts.onRendered(function() {
+    var ctx  = document.getElementById("myChart").getContext("2d");
+    var data = [
+    {
+        value: 300,
+        color:"#F7464A",
+        highlight: "#FF5A5E",
+        label: "Red"
+    },
+    {
+        value: 50,
+        color: "#46BFBD",
+        highlight: "#5AD3D1",
+        label: "Green"
+    },
+    {
+        value: 100,
+        color: "#FDB45C",
+        highlight: "#FFC870",
+        label: "Yellow"
+    }
+    ]
+    var myPieChart = new Chart(ctx).Pie(data);
+});
